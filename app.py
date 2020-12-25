42898fcb682bb131e8f0461f6d11b18691d7a77f
abrarsjq <abrar.sjq@gmail.com>
2020-12-25 19:48:43 +0300

    API endpoints builded successfully with models

--- app.py
+++ app.py
@@ -2,15 +2,196 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db, Actor, Movie
+
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+    # configure the app with the endpoints and return it
 
-  return app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PATCH,POST,DELETE,OPTIONS')
+
+        return response
+
+    # Endpoint to retrieve all actors
+    @app.route('/actors')
+    def get_actors():
+        actors = Actor.query.all()
+        if len(actors) == 0:
+            # abort the request, becasue no record retrieved
+            abort(404)
+
+        return jsonify({'success': True, 'actors':
+                       [actor.format() for actor in actors]})
+
+    # Endpoint to delete actor with requested ID
+    @app.route('/actors/<int:actor_id>', methods=['DELETE'])
+    def delete_actor(actor_id):
+        actor = Actor.query.get(actor_id)
+        if actor is None:
+            # abort the request, becasue no actor with that ID
+            abort(404)
+
+        actor.delete()
+
+        return jsonify({'success': True, 'actor_id': actor_id})
+
+    # Endpoint to add new actor to the database
+    @app.route('/actors', methods=['POST'])
+    def create_actor():
+        data = request.get_json()
+
+        if 'name' not in data:
+            # if name not exist in the request body, abort
+            abort(400)
+        if 'age' not in data:
+            # if age not exist in the request body, abort
+            abort(400)
+        if 'gender' not in data:
+            # if gender not exist in the request body, abort
+            abort(400)
+
+        actor = Actor(name=data['name'],
+                      gender=data['gender'],
+                      age=data['age'])
+        actor.insert()
+
+        return jsonify({'success': True,
+                        'actor': actor.format(),
+                        'actor_id': actor.id})
+
+    # Endpoint to edit the data of existing actor
+    @app.route('/actors/<int:actor_id>', methods=['PATCH'])
+    def edit_actor(actor_id):
+        data = request.get_json()
+
+        actor = Actor.query.get(actor_id)
+
+        if actor is None:
+            # abort the request, becasue no actor with that ID
+            abort(404)
+
+        # if one of these property exist, mean we need to update
+        if 'name' in data:
+            actor.name = data['name']
+
+        if 'gender' in data:
+            actor.gender = data['gender']
+
+        if 'age' in data:
+            actor.age = data['age']
+
+        # update the database record
+        actor.update()
+
+        return jsonify({'success': True,
+                        'actor': actor.format(),
+                        'actor_id': actor_id})
+
+
+    # Endpoint to retrieve all movies
+    @app.route('/movies')
+    def get_movies():
+        movies = Movie.query.all()
+
+        # abort the request, becasue no record retrieved
+        if len(movies) == 0:
+            abort(404)
+
+        return jsonify({'success': True,
+                       'movies': [movie.format() for movie in movies]})
+
+    # Endpoint to delete movie with requested ID
+    @app.route('/movies/<int:movie_id>', methods=['DELETE'])
+    def delete_movie(movie_id):
+        movie = Movie.query.get(movie_id)
+
+        if movie is None:
+            # abort the request, becasue no movie with that ID
+            abort(404)
+
+        movie.delete()
+
+        return jsonify({'success': True, 'movie_id': movie_id})
+
+    # Endpoint to add new movie to the database
+    @app.route('/movies', methods=['POST'])
+    def create_movie():
+        data = request.get_json()
+
+        if 'title' not in data:
+            # if movie title not exist in the request body, abort
+            abort(400)
+        if 'release' not in data:
+            # if release date not exist in the request body, abort
+            abort(400)
+
+        movie = Movie(title=data['title'], release=data['release'])
+        movie.insert()
+
+        return jsonify({'success': True,
+                        'movie': movie.format(),
+                        'movie_id': movie.id})
+
+    # Endpoint to edit the data of existing movie
+    @app.route('/movies/<int:movie_id>', methods=['PATCH'])
+    def edit_movie(movie_id):
+        data = request.get_json()
+
+        movie = Movie.query.get(movie_id)
+
+        if movie is None:
+            # abort the request, becasue no movie with that ID
+            abort(404)
+
+        # if one of these property exist in the request, mean we need to update
+        if 'title' in data:
+            movie.title = data['title']
+
+        if 'release' in data:
+            movie.release = data['release']
+
+        # update the database record
+        movie.update()
+
+        return jsonify({'success': True,
+                        'movie': movie.format(),
+                        'movie_id': movie_id})
+
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return (jsonify({'success': False,
+                         'error': 404,
+                         'message': 'resource not found'}),
+                404)
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return (jsonify({'success': False,
+                         'error': 400,
+                         'message': 'bad request'}),
+                400)
+
+    @app.errorhandler(401)
+    def unauthorized(error):
+        return (jsonify({'success': False,
+                         'error': 401,
+                         'message': 'unauthorized access'}),
+                401)
+
+    return app
+
 
 APP = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    APP.run(debug=True)
--- models.py
+++ models.py
@@ -1,1 +1,95 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy import Integer, String, Column
 
+try:
+    database_path = os.environ['DATABASE_URL']
+except:
+    database_name = "agency"
+    database_path = "postgresql://{}:{}@{}/{}".format('postgres',
+                                                    'admin',
+                                                    'localhost:5432',
+                                                    database_name)
+db = SQLAlchemy()
+
+
+def setup_db(app, database_path=database_path):
+    # complete the database configuration and connect the app
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    #db.create_all()
+
+
+class Actor(db.Model):
+    # Model describe the actor
+    __tablename__ = 'actor'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"<Actor id='{self.id}' name='{self.name}'>"
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+
+class Movie(db.Model):
+    # Model describe the movie
+    __tablename__ = 'movie'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    release = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"<Movie id='{self.id}' title='{self.title}'>"
+
+    def __init__(self, title, release):
+        self.title = title
+        self.release = release
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release': self.release,
+        }
